bf6bda062a371c7644dbffcd94bca9496ce61192
unknown <ahopkins@ANDYH.afs-uk.org>
2015-02-26 16:36:18 +0000

    Add Timing to classifier

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -31,9 +31,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 clf = GaussianNB()
+t0 = time()
 clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0,3),"s"
+t1 = time()
 pred = clf.predict(features_test)
-
+print "Prediction time:", round(time()-t1,3),"s"
 accuracy = accuracy_score(pred, labels_test)
 
 print accuracy
