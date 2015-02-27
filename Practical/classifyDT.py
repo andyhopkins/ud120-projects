d2094dca684da187bacb59a6cf40b12d3615e7f9
unknown <ahopkins@ANDYH.afs-uk.org>
2015-02-27 15:52:00 +0000

    Lesson 3 mini project part 1 Decision tree on emails

--- Practical/classifyDT.py
+++ Practical/classifyDT.py
@@ -2,7 +2,7 @@ def classify(features_train, labels_train):
     
     ### your code goes here--should return a trained decision tree classifer
     from sklearn import tree
-    clf = tree.DecisionTreeClassifier()
+    clf = tree.DecisionTreeClassifier(min_samples_split = 50)
     clf = clf.fit(features_train, labels_train)
     
     return clf
--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -26,7 +26,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
 
+clf = tree.DecisionTreeClassifier(min_samples_split = 40)
+t0 = time()
+clf = clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+pred = clf.predict(features_test)
+print "Prediction time:", round(time()-t0, 3), "s"
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print "Accuracy is ",acc * 100,"%"
 
 #########################################################
 
