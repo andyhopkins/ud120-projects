eb040b12010d45fec470046a7b5254d1edba6624
Andy Hopkins <andyhopkins2003@gmail.com>
2015-03-03 23:13:56 +0000

    Lesson 10 Parse 200 emails and remove words Creates list files for words and senders

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,21 +41,27 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
         temp_counter += 1
-        if temp_counter < 200:
+        if temp_counter < 200 :
             path = os.path.join('..', path[:-1])
-            print path
+            #print path
             email = open(path, "r")
-
+            
             ### use parseOutText to extract the text from the opened email
-
+            words = parseOutText(email)
+            print words
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
+            replaceWords = ["sara", "shackleton", "chris", "germani"]
+            for w in replaceWords:
+                words = words.replace(w,"")
             ### append the text to word_data
-
+            word_data.append(words)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
-
+            if from_person == "sara":
+                from_data.append(0)
+            elif from_person == "chris":
+               from_data.append(1)
+            
             email.close()
 
 print "emails processed"
