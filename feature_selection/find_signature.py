03832b840da282ac0db16d1ff7e6e709f93b4abb
unknown <ahopkins@ANDYH.afs-uk.org>
2015-03-04 13:48:30 +0000

    Lesson 11 Feature selection

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -6,8 +6,8 @@ numpy.random.seed(42)
 
 
 ### the words (features) and authors (labels), already largely processed
-words_file = "word_data_overfit.pkl" ### like the file you made in the last mini-project 
-authors_file = "email_authors_overfit.pkl"  ### this too
+words_file = "../text_learning/your_word_data.pkl" ### like the file you made in the last mini-project 
+authors_file = "../text_learning/your_email_authors.pkl"  ### this too
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
 
@@ -16,6 +16,7 @@ authors = pickle.load( open(authors_file, "r") )
 ### test_size is the percentage of events assigned to the test set (remainder go into training)
 from sklearn import cross_validation
 features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+
 
 
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -35,5 +36,24 @@ labels_train   = labels_train[:150]
 
 ### your code goes here
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+
+pred = clf.predict(features_test)
+
+acc = accuracy_score(pred,labels_test)
 
 
+print 'Length is ',len(features_train)
+print 'Accuracy is ',acc
+
+topFeatures = clf.feature_importances_
+
+cnt = 0
+for value in topFeatures:
+    if value > 0.2:
+        print 'value nuber ',cnt,' value ',value,' word ', vectorizer.get_feature_names()[cnt]
+    cnt += 1
+    
--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -42,7 +42,7 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
         temp_counter += 1
-        if temp_counter < 2000000000 :
+        if temp_counter < 2000000 :
             path = os.path.join('..', path[:-1])
             #print path
             email = open(path, "r")
@@ -51,15 +51,15 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             words = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-            replaceWords = ["sara", "shackleton", "chris", "germani"]
+            replaceWords = ["sara", "shackleton", "chris", "germani","sshacklensf","cgermannsf"]
             for w in replaceWords:
                 words = words.replace(w,"")
             ### append the text to word_data
             word_data.append(words)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-            if from_person == "sara":
+            if name == "sara":
                 from_data.append(0)
-            elif from_person == "chris":
+            elif name == "chris":
                from_data.append(1)
             
             email.close()
@@ -82,7 +82,7 @@ vectorizer = TfidfVectorizer(stop_words="english")
 
 vocab_list = vectorizer.fit_transform(word_data)
 print "#feature names: ", len(vectorizer.get_feature_names())
-print vocab_list[34597]
+
 
 
 
